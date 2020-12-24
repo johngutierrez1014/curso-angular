4641d50949367f9215737da2f59392f10588af3d
JohnGutierrez <johngutierrez1014@gmail.com>
2020-12-24 15:37:32 -0500

    Manejo de rutas

--- angular-wishlist/src/app/app.module.ts
+++ angular-wishlist/src/app/app.module.ts
@@ -1,18 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { DestinoViajeComponent } from './destino-viaje/destino-viaje.component';
 import { ListaDestinoComponent } from './lista-destino/lista-destino.component';
+import { from } from 'rxjs';
+import { DestinoDetalleComponent } from './destino-detalle/destino-detalle.component';
+
+/**Barra de navegacion */
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: ListaDestinoComponent},
+  { path: 'destino', component: DestinoDetalleComponent}
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     DestinoViajeComponent,
-    ListaDestinoComponent
+    ListaDestinoComponent,
+    DestinoDetalleComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
